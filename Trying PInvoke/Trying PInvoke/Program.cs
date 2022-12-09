79e612527797d75b833ce10f6cd0f9b1c2d4d603
Dominykas <97787345+aquajay2022@users.noreply.github.com>
2022-12-09 12:17:01 +0200

    Add files via upload

---
+++ Trying PInvoke/Trying PInvoke/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Trying_PInvoke
+{
+    internal class Program
+    {
+        public class ClassyClass
+        {
+            public override string ToString()
+            {
+                return $"{this.id}\n{this.phone}";
+            }
+            public string phone { get; set; }
+            public int id { get; set; }
+        }
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+
+        static async Task Main(string[] args)
+        {
+            ClassyClass result;
+            using (HttpClient client = new HttpClient())
+            {
+                Uri endpoint = new Uri("https://jsonplaceholder.typicode.com/users/1");
+                result = await client.GetFromJsonAsync<ClassyClass>(endpoint);
+            }
+            MessageBox(IntPtr.Zero, result.ToString(), "Results", 0);
+            StreamWriter sr = new StreamWriter("C:\\Users\\Dominylas\\Desktop\\SWSR\\Whatever.txt");
+            sr.Write(result.ToString());
+            sr.Close();
+            Console.ReadKey();
+        }
+    }
+}
